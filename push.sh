280fe5c8ca05538f1b531937b21b100116373229
Eric Jacobson <contact@efjacobson.com>
2022-07-04 17:24:16 -0700

    inteligent push

--- push.sh
+++ push.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 if [[ `source get-redacted-count.sh` != 0 ]]; then
-    echo "you didnt restore the passwords"
-    exit 1
+    if [ ! -f ".passwords" ]; then
+        echo "passwords redacted and nothing to restore them from"
+        sleep 3
+        exit 1
+    else
+        source $(pwd)/restore-passwords.sh
+    fi
 fi
 
 cp etc/rc.local.sh etc/rc.local
 scp etc/profile etc/rc.local root@192.168.1.1:/etc/
+ssh root@192.168.1.1 chmod 644 /etc/profile
 rm etc/rc.local
